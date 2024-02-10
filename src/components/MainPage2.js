1a8f91b141a03f621ef765f1feefe213e0c4170f
sungyu0309 <tjsrb439@handong.ac.kr>
2024-02-11 01:35:59 +0900

    style : 푸터 제외한 메인페이지 css 추가완료

--- src/components/MainPage2.js
+++ src/components/MainPage2.js
@@ -49,9 +49,94 @@ function MainPage2() {
           </p>
         </div>
       </div>
-      <div className={styles.announcement}>교내공지</div>
+      <div className={styles.announcement}>
+        <h4>교내공지</h4>
+        <ol className={styles.flex2}>
+          <div>
+            <li>2024 SW 페스티벌 개최 안내</li>
+            <li>2024 SW 창업 경진 대회 안내</li>
+            <li>실전! 머신러닝 문제해결 캠프 안내</li>
+          </div>
+          <div>
+            <li>THE CEO 공모전 안내</li>
+            <li>제 2회 학생 주도형 SW 해커톤 경진 대회</li>
+            <li>트레이딩 머신 프로젝트</li>
+          </div>
+          <div>
+            <li>KT AIVLE School 4기 교육학교 추천</li>
+            <li>대경권 SW 산학 프로젝트 경진대회</li>
+            <li>제 10회 소개팅 안내</li>
+          </div>
+        </ol>
+      </div>
       <div className={styles.section2}>
-        팀원이 찾기 막막할 땐 우리가 도와줄게
+        <p>
+          팀원이 찾기 막막할 땐<br /> 우리가 도와줄게
+        </p>
+        <p>
+          필유인은 교내활동부터 교외활동까지 다양한 여정을 함께 할 최적의 팀원을
+          찾아드립니다
+        </p>
+        <img src="img/fill-you-in-logo-blue.png" alt="logo" />
+      </div>
+      <div className={styles.section3}>
+        <div className={styles.whiteCircle}>
+          <img src="img/Line 122.png" alt="line" className={styles.line122} />
+          <div className={styles.circle1}></div>
+          <div className={styles.circle2}>
+            <div className={styles.circle3}></div>
+            <p className={styles.title}>
+              채움이 있는
+              <br />
+              대학생활
+            </p>
+            <p className={styles.explanation}>
+              이력관리의 불편함
+              <br />
+              팀원 모집과 대외활동 참여의 어려움
+            </p>
+          </div>
+          <img src="img/Line 123.png" alt="line" className={styles.line123} />
+        </div>
+
+        <div className={styles.blueCircle}>
+          <div className={styles.circle1}>
+            <p>필유인</p>
+          </div>
+        </div>
+
+        <div className={styles.frame}>
+          <div className={styles.frame1}>
+            <p className={styles.title}>나의 이력을 간편하게</p>
+            <p className={styles.explanation}>
+              필유인으로 수업 수강내역, 활동 및<br />
+              수상내역을 쉽게 관리해보세요
+            </p>
+          </div>
+          <div className={styles.frame2}>
+            <p className={styles.title}>나의 활동을 가득차게</p>
+            <p className={styles.explanation}>
+              나에게 필요한 교내 및 교외활동을
+              <br />
+              원하는 사람들과 참여해보세요
+            </p>
+          </div>
+          <div className={styles.frame3}>
+            <p className={styles.title}>교내 네트워킹을 활발하게</p>
+            <p className={styles.explanation}>
+              교내 사람들의 프로필을 확인하고
+              <br />
+              직접 컨택하거나, 소통해보세요
+            </p>
+          </div>
+        </div>
+
+        <p className={styles.summary}>
+          채움이 있는 대학생활,
+          <br />
+          필유인에서 경험할 수 있습니다
+        </p>
+        <button className={styles.findTeamMate}>나에게 맞는 팀원 찾기</button>
       </div>
     </div>
   );
