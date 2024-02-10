1d6d6f0e6ece30416e9e43dcf33994c3d4fa9349
ejPark43 <pej22200310@handong.ac.kr>
2024-02-10 23:01:05 +0900

    style: 구글 로그인 버튼 호버 추가 및 불필요한 코드 삭제

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -10,10 +10,8 @@ const Container = styled.div`
 
   > div {
     background-color: #ffffff;
-    /*  */
     height: 100%;
     /* overflow: hidden; */
-    /*   오버플로우가 Hidden이면 그거 오른쪽에 있는 디자인들이 사라지게됨... 크기 줄여서 맞추던가 해야할듯*/
     position: relative;
     width: 100%;
   }
@@ -21,22 +19,16 @@ const Container = styled.div`
 
 const Header = styled.div`
   display: flex;
-  /* align-items: center; */
+
   width: 100vw;
   height: 100px;
-  /* border: 2px solid blue; */
+
   > img {
     margin-left: 3.8vw;
     margin-top: 1.67vw;
-    /* left: 54px;
-    position: absolute;
-    top: 32px; */
+
     height: 62px;
     width: 232px;
-  }
-  > .fill-you-in-logo {
-    //fill you in logo
-    /* border: 2px solid red; */
   }
 `;
 const TextWrapper = styled.div`
@@ -45,13 +37,12 @@ const TextWrapper = styled.div`
   font-family: "Pretendard-Bold", Helvetica;
   font-size: 70px;
   font-weight: 700;
-  /* margin-left: 10%;
-  margin-top: 10%; */
+
   left: 103px;
   letter-spacing: 0;
-  /* line-height: 120px; */
+
   position: absolute;
-  /* top: 217px; */
+
   top: 182px;
 `;
 
@@ -65,14 +56,12 @@ const ShortP = styled.p`
   letter-spacing: 0;
   line-height: normal;
   position: absolute;
-  /* top: 498px; */
-  /* top: 430px; */
+
   top: 380px;
   white-space: nowrap;
 `;
 
 const Bubbles = styled.div`
-  /* height: 909px; */
   height: 660px;
   overflow: hidden;
   width: 52%;
@@ -95,10 +84,6 @@ const Bubbles = styled.div`
 
     transform: rotate(-125.11deg);
     overflow: hidden;
-    /* 
-        top: 269px;
-        height: 263px;
-    width: 263px; */
     top: 206px;
     height: 200px;
     width: 200px;
@@ -110,14 +95,8 @@ const Bubbles = styled.div`
       rgba(14, 158, 158, 0.19) 96%
     );
     border-radius: 179px;
-
     left: 220px;
     position: absolute;
-
-    /*top: 551px;
-     height: 358px;
-    width: 358px; */
-    /* top: 488px; */
     top: 425px;
     height: 295px;
     width: 295px;
@@ -133,10 +112,6 @@ const Bubbles = styled.div`
     left: 411px;
     position: absolute;
     top: 10px;
-    /* height: 459px;
-    width: 459px; */
-    /* height: 396px;
-    width: 396px; */
     height: 370px;
     width: 370px;
   }
@@ -150,10 +125,6 @@ const Bubbles = styled.div`
     left: 399px;
     position: absolute;
 
-    /* 
-     top: 381px;
-     height: 355px;
-    width: 355px; */
     top: 318px;
     height: 292px;
     width: 292px;
@@ -165,11 +136,6 @@ const Bubbles = styled.div`
     border-radius: 299px;
     box-shadow: inset -28.4px 28.4px 20px #ffffff1a, 0px 4px 20px #0000001a;
     position: absolute;
-    /* 
-      left: 0;
-     top: 381px;
-     height: 225px;
-    width: 225px; */
     left: 63px;
     top: 300px;
     height: 162px;
@@ -181,10 +147,12 @@ const LoginGroup = styled.div`
   height: 56px;
   left: 98px;
   position: absolute;
-  /* top: 560px; */
-  /* top: 525px; */
+
   top: 470px;
   width: 291px;
+  &:hover {
+    cursor: pointer;
+  }
 
   > .overlap-group {
     background-color: #06b5b5;
@@ -192,6 +160,11 @@ const LoginGroup = styled.div`
     height: 56px;
     position: relative;
     width: 289px;
+    transition: 0.2s;
+    &:hover {
+      background-color: black;
+      cursor: pointer;
+    }
   }
   > .overlap-group > .text-wrapper-2 {
     color: #ffffff;
@@ -215,6 +188,11 @@ const LoginGroup = styled.div`
     width: 36px;
   }
 `;
+
+const googleButton = () => {
+  console.log("BUTTON CLICKEd");
+};
+
 export const LoginScreen = () => {
   return (
     <Container className="index">
@@ -229,16 +207,11 @@ export const LoginScreen = () => {
         </Header>
 
         <TextWrapper>
-          {/* <div className="text-wrapper"> */}
           채움이 있는 대학생활,
           <br />
           필유인 입니다.
-          {/* </div> */}
         </TextWrapper>
-
-        {/* <p className="p"> */}
         <ShortP>나의 이력을 관리하고 원하는 팀을 구성할 수 있어요.</ShortP>
-        {/* <div className="overlap"> */}
         <Bubbles>
           <div className="ellipse" />
           <div className="ellipse-2" />
@@ -246,9 +219,7 @@ export const LoginScreen = () => {
           <div className="ellipse-4" />
           <div className="ellipse-5" />
         </Bubbles>
-        {/* </div> */}
-        <LoginGroup>
-          {/* <LoginGroup className="group"> */}
+        <LoginGroup onClick={googleButton}>
           <div className="overlap-group">
             <div className="text-wrapper-2">Google로 로그인</div>
             <img
