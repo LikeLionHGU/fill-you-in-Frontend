b8903582c2c303b36160b1f69c42d178a9e2ccf9
sungyu0309 <tjsrb439@handong.ac.kr>
2024-02-20 01:08:21 +0900

    chore : Mainpage 이름 변경

--- src/RouteSet.js
+++ src/RouteSet.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginScreen from "./components/LoginScreen";
-import MainPage2 from "./components/MainPage2";
+import MainPage from "./components/MainPage";
 import MyPage from "./components/MyPage";
 function RouteSet() {
   return (
     <Router>
       <Routes>
-        <Route path="/MainPage" element={<MainPage2 />}></Route>
+        <Route path="/MainPage" element={<MainPage />}></Route>
         <Route path="/MyPage" element={<MyPage />}></Route>
         <Route path="/" element={<LoginScreen />}></Route>
       </Routes>
