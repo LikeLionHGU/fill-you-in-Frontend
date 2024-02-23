31047cab5596caf2e01b37f0f44d40a524db63ef
ejPark43 <pej22200310@handong.ac.kr>
2024-02-24 01:40:51 +0900

    feat: 오류 처리

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -96,89 +96,6 @@ const ShortP = styled.p`
   white-space: nowrap;
 `;
 
-// const Bubbles = styled.div`
-//   // 디자인 //
-//   height: 660px;
-//   overflow: hidden;
-//   width: 52%;
-//   display: flex; //
-//   left: 50%;
-//   position: absolute;
-//   top: 12vw;
-
-//   > .ellipse {
-//     // bubble 1
-//     background: linear-gradient(
-//       180deg,
-//       rgb(9, 186, 186) 0%,
-//       rgba(14, 158, 158, 0.19) 96%
-//     );
-//     border-radius: 131.5px;
-
-//     left: 131px;
-//     position: absolute;
-
-//     transform: rotate(-125.11deg);
-//     overflow: hidden;
-//     top: 206px;
-//     height: 200px;
-//     width: 200px;
-//   }
-//   > .ellipse-2 {
-//     background: linear-gradient(
-//       180deg,
-//       rgb(9, 186, 186) 0%,
-//       rgba(14, 158, 158, 0.19) 96%
-//     );
-//     border-radius: 179px;
-//     left: 220px;
-//     position: absolute;
-//     top: 425px;
-//     height: 295px;
-//     width: 295px;
-//   }
-//   > .ellipse-3 {
-//     background: linear-gradient(
-//       180deg,
-//       rgb(9, 186, 186) 0%,
-//       rgba(14, 158, 158, 0.19) 96%
-//     );
-//     border-radius: 229.5px;
-
-//     left: 411px;
-//     position: absolute;
-//     top: 10px;
-//     height: 370px;
-//     width: 370px;
-//   }
-//   > .ellipse-4 {
-//     -webkit-backdrop-filter: blur(20px) brightness(100%);
-//     backdrop-filter: blur(20px) brightness(100%);
-//     background-color: #d9d9d91a;
-//     border-radius: 299px;
-//     box-shadow: inset -28.4px 28.4px 20px #ffffff1a, 0px 4px 20px #0000001a;
-
-//     left: 399px;
-//     position: absolute;
-
-//     top: 318px;
-//     height: 292px;
-//     width: 292px;
-//   }
-//   > .ellipse-5 {
-//     -webkit-backdrop-filter: blur(20px) brightness(100%);
-//     backdrop-filter: blur(20px) brightness(100%);
-//     background-color: #d9d9d91a;
-//     border-radius: 299px;
-//     box-shadow: inset -28.4px 28.4px 20px #ffffff1a, 0px 4px 20px #0000001a;
-//     position: absolute;
-//     left: 63px;
-//     top: 300px;
-//     height: 162px;
-//     width: 162px;
-//   }
-// `;
-
 const LoginGroup = styled.div`
   height: 56px;
   left: 98px;
@@ -236,7 +153,7 @@ export const LoginScreen = () => {
     const url = process.env.REACT_APP_BACK_URL + "/api/fillyouin/auth/login"; // 백엔드 api url
 
     const token = credentialIdToken;
-    console.log("credential: ", token);
+
     const data = {
       googleIdToken: token, // 백으로 넘겨줄 credential 토큰
     };
@@ -254,14 +171,15 @@ export const LoginScreen = () => {
       }
 
       const responseData = await response.json();
-      console.log("Server Response", responseData);
-      console.log(responseData.jwt); // 백엔드에서 전달해준 jwt 토큰
+
       if (responseData.jwt) {
         // console.log("jwt 존재함");
         localStorage.setItem("loginToken", responseData.jwt); // 로컬에 해당 Jwt를 저장해줌
       }
     } catch (error) {
       console.error("error", error);
+      alert("로그인에 오류가 발생했습니다. 다시 시도해주세요");
+      navigate("/");
     }
   };
   const navigate = useNavigate();
@@ -272,7 +190,6 @@ export const LoginScreen = () => {
   };
   useEffect(() => {
     checkLogin();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return loggedIn ? (
@@ -290,7 +207,6 @@ export const LoginScreen = () => {
                   alt="logoImg"
                 />
               </Header>
-
               <TextWrapper>
                 채움이 있는 대학생활,
                 <br />
@@ -299,35 +215,19 @@ export const LoginScreen = () => {
               <ShortP>
                 나의 이력을 관리하고 원하는 팀을 구성할 수 있어요.
               </ShortP>
-              {/* <Bubbles>
-            <div className="ellipse" />
-            <div className="ellipse-2" />
-            <div className="ellipse-3" />
-            <div className="ellipse-4" />
-            <div className="ellipse-5" />
-          </Bubbles> */}
 
               <LoginGroup onClick={googleButton}>
-                {/* 구글 로그인 버튼.... 
-          <div className="overlap-group">
-            <div className="text-wrapper-2">Google로 로그인</div>
-            <img
-              className="google-logo"
-              alt="Element"
-              src="https://cdn.animaapp.com/projects/65c5a7d8d4b749ab51e73dc0/releases/65c5a8a67211e272ff217379/img/-------2-500-1.png"
-            />
-          </div> */}
-
                 <GoogleLogin
                   className="google-login-btn"
                   onSuccess={(credentialResponse) => {
-                    // console.log(credentialResponse); // 로그인한 객체 전부 가져옴
                     getLoginData(credentialResponse.credential).then(() => {
                       navigate("/MainPage");
                     }); // 로그인 성공하면 유저 credential 정보를 넘겨줌
                   }}
                   onError={() => {
-                    alert("로그인에 실패했습니다.");
+                    console.log("loginToken: ", loginToken);
+                    alert("로그인에 실패했습니다. 다시 시도해주세요");
+                    navigate("/");
                     console.log("Login Failed");
                   }}
                 />
