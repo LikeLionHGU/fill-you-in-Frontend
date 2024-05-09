491a33337f18e62aa2019fdaadfd059e675e3b55
sungyu0309 <tjsrbyt@gmail.com>
2024-05-09 16:41:23 +0900

    style : 폴더 추가 버튼 css 추가

--- src/components/WriteActivity/AddBtnComponent.js
+++ src/components/WriteActivity/AddBtnComponent.js
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+import PlusImg from "../../img/Vector.png";
+
+const AddBtn = styled.button`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: none;
+  background-color: #04b1b1;
+
+  > img {
+    width: 20px;
+    padding-top: 3px;
+  }
+`;
 
 export default function AddBtnComponent() {
   return (
-    <button onClick={() => console.log("add Btn clicked")}>추가 버튼</button>
+    <AddBtn onClick={() => console.log("add Btn clicked")}>
+      <img src={PlusImg} alt="plusImg" />
+    </AddBtn>
   );
 }
