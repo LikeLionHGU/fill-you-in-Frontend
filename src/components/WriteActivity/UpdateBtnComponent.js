032a777d9d13b91efbb8cde4b2db50fd0ed24f95
sungyu0309 <tjsrbyt@gmail.com>
2024-05-13 17:34:02 +0900

    style : 수정, 삭제 버튼 추가

--- src/components/WriteActivity/UpdateBtnComponent.js
+++ src/components/WriteActivity/UpdateBtnComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import updateBtn from "../../img/dot.png";
 import styled from "styled-components";
 
@@ -13,9 +13,51 @@ const UpdateBtn = styled.button`
 `;
 
 export default function UpdateBtnComponent() {
+  const [showOption, setShowOption] = useState(false);
   return (
-    <UpdateBtn>
-      <img src={updateBtn} alt="updateBtn" />
-    </UpdateBtn>
+    <>
+      <UpdateBtn
+        onFocus={() => setShowOption(true)}
+        onBlur={(e) => {
+          const modifyBtn = document.querySelector("#modify");
+          const deleteBtn = document.querySelector("#delete");
+          if (e.relatedTarget !== modifyBtn && e.relatedTarget !== deleteBtn) {
+            setShowOption(false);
+          }
+        }}
+      >
+        <img src={updateBtn} alt="updateBtn" />
+      </UpdateBtn>
+      <ShowModifyOption show={showOption} setShow={setShowOption} />
+    </>
+  );
+}
+
+function ShowModifyOption({ show, setShow }) {
+  return (
+    <>
+      {show && (
+        <>
+          <button
+            id="modify"
+            onClick={() => {
+              setShow(false);
+              console.log("modify clicked");
+            }}
+          >
+            수정
+          </button>
+          <button
+            id="delete"
+            onClick={() => {
+              setShow(false);
+              console.log("delete clicked");
+            }}
+          >
+            삭제
+          </button>
+        </>
+      )}
+    </>
   );
 }
